a3029c13b27b1cc2eac43493491e8709f081b2b3
coocooder1 <elvs97121041@gmail.com>
2024-08-27 14:21:53 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Banner from "./components/Banner";
+import Cook from "./components/Cook";
+import Expert from "./components/Expert";
+import Gallery from "./components/Gallery";
+import Newsletter from "./components/Newsletter";
+import Work from "./components/Work";
+
+
+export default function Home() {
+  return (
+    <main >
+      <Banner/>
+      <Work/>
+      <Cook/>
+      <Expert/>
+      <Gallery/>
+      <Newsletter/>
+     
+    </main>
+  );
+}
